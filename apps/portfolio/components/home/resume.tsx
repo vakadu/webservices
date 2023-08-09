d7a5d9f10090b936dd083ecc70b75997b418105b
vakadu <vakadu10@gmail.com>
2023-08-09 09:00:27 +0530

    Update resume.tsx

--- apps/portfolio/components/home/resume.tsx
+++ apps/portfolio/components/home/resume.tsx
@@ -1,5 +1,48 @@
 import { PortfolioSectionHeader } from "@webservices/ui";
 
+const data = [
+    {
+        id: 1,
+        title: "Indian Institue of Technology - Roorkee",
+        company: "Msc.Int.Chemistry",
+        dec: "",
+        startYear: "2010",
+        endYear: "2015",
+    },
+    {
+        id: 2,
+        title: "Software Developer",
+        company: "Kwiqpick Services India Pvt. Ltd",
+        dec: "",
+        startYear: "2017",
+        endYear: "2018",
+    },
+    {
+        id: 3,
+        title: "Frontend Engnieer",
+        company: "Arzooo",
+        dec: "",
+        startYear: "2018",
+        endYear: "2019",
+    },
+    {
+        id: 4,
+        title: "Frontend Engnieer - 2",
+        company: "Arzooo",
+        dec: "",
+        startYear: "2019",
+        endYear: "2021",
+    },
+    {
+        id: 5,
+        title: "Tech Lead(Frontend)",
+        company: "Arzooo",
+        dec: "",
+        startYear: "2021",
+        endYear: "2023",
+    },
+];
+
 const Resume = () => {
     const headerClasses = "text-center capitalize text-24 font-medium pb-24 border-b-2 border-black";
 
@@ -12,12 +55,22 @@ const Resume = () => {
                 <PortfolioSectionHeader
                     title="resume"
                 />
-                <div className="grid grid-cols-2 lg:gap-42">
-                    <div className="col-span-1">
-                        <h5 className={headerClasses}>education</h5>
-                    </div>
-                    <div className="col-span-1">
-                        <h5 className={headerClasses}>experience</h5>
+                <div className="">
+                    <h5 className={headerClasses}>education & experience</h5>
+                    <div>
+                        {
+                            data.map((res) => {
+                                return(
+                                    <div
+                                        key={res.id}
+                                    >
+                                        <h6>
+                                            { res.title }
+                                        </h6>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </article>
