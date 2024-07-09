05ae2b28a5db4f708ad76df70ba5e27e58d54308
pemilytech <vakadu10@gmail.com>
2024-07-09 17:31:35 +0530

    added intial

---
+++ apps/interview-prep/app/circles/page.tsx
@@ -0,0 +1,8 @@
+// https://devtools.tech/questions/s/how-to-build-circles-game-in-react-js-frontend-coding-challenge---qid---Y8acly7B5CmIVAaT5knP
+
+export default function Index() {
+    return (
+        <div>
+        </div>
+    );
+}
--- apps/interview-prep/tailwind.config.js
+++ apps/interview-prep/tailwind.config.js
@@ -3,15 +3,30 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+		...createGlobPatternsForDependencies(__dirname),
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sathoshi: ['var(--font-sathoshi)'],
+			},
+			colors: {
+				brand: 'rgb(var(--color-brand) / <alpha-value>)',
+				secondary: '#040F0F',
+				teritary: '#2D3A3A',
+				beige: 'rgb(245, 239, 231)',
+				tomato: '#F06543',
+				black: {
+					bg: '#1C2428',
+					bg2: '#1C1B1E',
+				},
+				grey: {
+					bg: '#F5F7FA',
+				},
+			},
+		},
+	},
+	plugins: [],
 };
